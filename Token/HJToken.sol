2e20b3108f8d99588d3693964d9be0c09efdd1df
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-10-14 20:34:16 +0100

    Déploiement du contrat

---
+++ Token/HJToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20; // Version stable recommandée pour Sepolia
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HJToken is ERC20, Ownable {
+    
+    event TokensMinted(address indexed to, uint256 amount);
+
+    constructor()
+        ERC20(
+            "HJToken",
+            "HJT"        
+        ) Ownable(msg.sender)
+    {
+     
+        mint(msg.sender, 1_000_000 * 10**decimals());
+        emit TokensMinted(msg.sender, 1_000_000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+        emit TokensMinted(to, amount);
+    }
+}
