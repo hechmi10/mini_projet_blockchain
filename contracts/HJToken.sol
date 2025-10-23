c53cbad56b327f2863bca43559c3f3df448a0aff
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-10-23 18:02:54 +0100

    'Ajustement'

--- contracts/HJToken.sol
+++ contracts/HJToken.sol
@@ -1,25 +1,54 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30; // Version stable recommandée pour Sepolia
+pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract HJToken is ERC20, Ownable {
+contract HJToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    address public owner;
     
-    event TokensMinted(address indexed to, uint256 amount);
-
-    constructor()
-        ERC20(
-            "HJToken",
-            "HJT"        
-        ) Ownable(msg.sender)
-    {
-     
-        mint(msg.sender, 1000000 * 10**decimals());
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
     }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-        emit TokensMinted(to, amount);
+    
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        owner = msg.sender;
+        totalSupply = _initialSupply * 10 ** decimals;
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+    
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value, "Insufficient balance");
+        require(allowance[from][msg.sender] >= value, "Allowance exceeded");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
     }
 }
